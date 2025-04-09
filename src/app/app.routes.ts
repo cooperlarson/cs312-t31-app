399399f5bc96af0fe0369a494a99d7817a2e640e
blondeman <domo23000@gmail.com>
2025-04-09 13:41:19 -0600

    basic grid painting table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { BiographyPageComponent } from './biography/biography-page/biography-page.component';
+import { PaintingGridComponent } from './painting/painting-grid/painting-grid.component';
 
 export const routes: Routes = [
-  { path: 'about-us', component: BiographyPageComponent }
+  { path: 'about-us', component: BiographyPageComponent },
+  { path: 'painting-grid', component: PaintingGridComponent }
 ];
