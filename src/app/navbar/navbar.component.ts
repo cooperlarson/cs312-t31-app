115ff395dea274f914bcbe112172e18d5489487f
Cooper Larson <cooper.larson1@gmail.com>
2025-04-22 15:22:19 -0600

    routing patch

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { DataService } from '../../data-service';
 
 @Component({
   selector: 'app-navbar',
@@ -16,12 +15,5 @@ export class NavbarComponent {
   logo = {
     src: 'images/HueGrid_logo.png',
     alt: 'HueGrid Logo',
-  }
-  
-  constructor(private dataService: DataService) {}
-
-  async testClick() {
-    const result = await this.dataService.test();
-    console.log('Result:', result);
   }
 }
